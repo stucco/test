8987f5dfe18c04383b13085f32201b1ca29943c8
euf <euf@ornl.gov>
2014-06-10 23:55:11 -0400

    dump more logs to console

--- scripts/test-db.sh
+++ scripts/test-db.sh
@@ -30,6 +30,9 @@ ls -al /stucco/rt/stucco-topology
 echo "!!! /stucco/rt/stucco-topology/stucco-rt.log !!!"
 cat /stucco/rt/stucco-topology/stucco-rt.log
 
+echo "!!! /stucco/rt/stucco-topology/stucco-rt-errors.log !!!"
+cat /stucco/rt/stucco-topology/stucco-rt-errors.log
+
 curl 'http://localhost:8182/graphs' > graphs.out
 # {"version":"2.4.0","name":"Rexster: A Graph Server","graphs":["graph"],"queryTime":20.218999,"upTime":"0[d]:00[h]:15[m]:11[s]"}
 jq '.' graphs.out
