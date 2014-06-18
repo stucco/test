163f56b475dd6239f031722860e03946869833c4
euf <euf@ornl.gov>
2014-06-18 15:32:41 -0400

    bugfix to test restarting script - would not restart in some cases

--- rerun-test.sh
+++ rerun-test.sh
@@ -6,4 +6,9 @@ echo y | travis version
 
 travis login --github-token "c528321727cdc581f77554ba6dc3c62e5c63764d" #test robot account
 
+# cancel pending job (if any) and then wait a bit for it to actually stop.
+# This is because running jobs cannot be restarted, and canceling/restarting immediately back to back can apparently cause strange behavior.
+travis cancel -r stucco/test
+sleep 3
+
 travis restart -r stucco/test --debug
